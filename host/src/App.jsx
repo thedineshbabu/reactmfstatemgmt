984437868642b3f943542657be486c070607156c
Dineshbabu Manoharan <thedineshbabu@hotmail.com>
2022-01-01 11:31:56 -0600

    counter included in both host and nav

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Header from "nav/Header";
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: host</div>
-    <Header/>
-    {/* <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div> */}
-  </div>
-);
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>Name: host</div>
+      <Header count={count} clear={() => setCount(0)} />
+      <p className="text-center">You clicked {count} times</p>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => setCount(count + 1)}
+      >
+        Increment
+      </button>
+    </div>
+  );
+};
 ReactDOM.render(<App />, document.getElementById("app"));
--- nav/src/components/Header.jsx
+++ nav/src/components/Header.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const Header = () => {
+const Header = ({ count, clear }) => {
   return (
-    <div className='bg-blue-400 text-3xl p-2 container'>
+    <div className="bg-red-400 text-3xl p-2 container w-full">
       <h1>Awesome Header</h1>
+      <p>{count}</p>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={clear}
+      >
+        Clear
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
